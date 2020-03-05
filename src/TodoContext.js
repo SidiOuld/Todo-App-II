56d52902545389c042f3759e54304303d8c21800
Sidi Ould <sidiould1@gmail.com>
2020-03-04 19:31:58 -0600

    changed hero bg color

---
+++ src/TodoContext.js
@@ -0,0 +1,19 @@
+import React, { createContext, useState } from "react";
+
+export const TodoContext = createContext();
+
+export const TodoProvider = props => {
+  const [todos, setTodos] = useState([
+    {
+      id: Date.now(),
+      value: "Add Your Todos",
+      done: false
+    }
+  ]);
+
+  return (
+    <TodoContext.Provider value={[todos, setTodos]}>
+      {props.children}
+    </TodoContext.Provider>
+  );
+};
