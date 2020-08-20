b7c01b3409dc4d3ca28511cf1a015bc08d1e20c1
Sidi Ould <sidiould1@gmail.com>
2020-08-19 21:47:50 -0500

    fix to show text todo when only one todo

--- src/Components/TodoList.js
+++ src/Components/TodoList.js
@@ -8,13 +8,13 @@ export default function TodoList() {
   const [todos, setTodos] = useContext(TodoContext);
 
   // toggleDone
-  const toggleDone = todo => {
-    todos.map(_todo => (_todo === todo ? (_todo.done = !todo.done) : todo));
+  const toggleDone = (todo) => {
+    todos.map((_todo) => (_todo === todo ? (_todo.done = !todo.done) : todo));
     setTodos([...todos]);
   };
   // delete
-  const deleteTodo = todo => {
-    const _todos = todos.filter(_todo => _todo !== todo);
+  const deleteTodo = (todo) => {
+    const _todos = todos.filter((_todo) => _todo !== todo);
     setTodos(_todos);
   };
 
@@ -22,13 +22,15 @@ export default function TodoList() {
     <div>
       <div className="hero has-background-primary">
         <div className="hero-body has-text-centered">
-          <p className="title is-1 has-text-white">{todos.length} Todos</p>
+          <p className="title is-1 has-text-white">
+            {todos.length} {todos.length > 1 ? "todos" : "todo"}
+          </p>
         </div>
       </div>
 
       <section className="section">
         <div className="container">
-          {todos.map(todo => (
+          {todos.map((todo) => (
             <Todo
               key={todo.id}
               todo={todo}
